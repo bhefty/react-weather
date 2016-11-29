54ce3d6139dd7fa5efcdffc9bca39ac4505ea5a5
Bill Hefty <billhefty@gmail.com>
2016-11-28 22:40:40 -0600

    Add wrapper div to About render

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 
 let About = (props) => {
   return (
-    <h3>About</h3>
-    <p>Welcome to the about page</p>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page</p>
+    </div>
   )
 }
 
