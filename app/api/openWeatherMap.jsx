a23a5e9be70e5b891fd0bc22879bce5b755984a4
Bill Hefty <billhefty@gmail.com>
2016-11-29 21:49:42 -0600

    Remove .evn reference

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=' + process.env.OPENWEATHER_API + '&units=imperial'
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c8c54ce510c3a7dcbe3aa01d56a66095&units=imperial'
 
 function getTemp(location) {
   let encodedLocation = encodeURIComponent(location)
