7b40c23e4db5feed456ec6fbbfdaf5e3243e9d25
Bill Hefty <billhefty@gmail.com>
2016-11-30 19:25:15 -0600

    Style About page and link to tools

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,24 @@ import React, { Component } from 'react'
 let About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className='text-center'>Aboout</h1>
+      <p>
+        This is a weather application on React. I have built this for
+        The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - This was the
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - I used
+            Open Weather Map to search for we
+        </li>
+      </ul>
     </div>
   )
 }
